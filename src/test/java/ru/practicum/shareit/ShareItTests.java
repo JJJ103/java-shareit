760e745996a35d847804b7c0ece6d41c43a9c476
JJJ0103 <jjj1.03@yandex.ru>
2024-11-12 15:57:19 +0300

    .

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ShareItApp.class)
+@SpringBootTest(classes = ShareItApp.class, properties = "spring.profiles.active=test")
 class ShareItTests {
 
 	@Test
