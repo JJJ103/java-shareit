413381dc1d0b6936e10e97cc6a0a88e50fdf350e
JJJ0103 <jjj1.03@yandex.ru>
2024-11-13 18:08:57 +0300

    добавлен отдельный BookingResponseMapper

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDTO;
+import ru.practicum.shareit.booking.mapper.BookingResponseMapper;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingState;
 
@@ -23,10 +24,8 @@ public class BookingController {
     @PostMapping
     public ResponseEntity<BookingResponseDTO> createBooking(@Valid @RequestBody BookingDto bookingDto,
                                                             @RequestHeader("X-Sharer-User-Id") Long userId) {
-
         Booking booking = bookingService.createBooking(userId, bookingDto);
-
-        BookingResponseDTO response = new BookingResponseDTO(booking);
+        BookingResponseDTO response = BookingResponseMapper.toBookingResponseDTO(booking);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -36,7 +35,7 @@ public class BookingController {
                                                      @RequestParam Boolean approved) {
         Booking booking = bookingService.confirmBooking(bookingId, userId, approved);
 
-        BookingResponseDTO response = new BookingResponseDTO(booking);
+        BookingResponseDTO response = BookingResponseMapper.toBookingResponseDTO(booking);
         return ResponseEntity.ok(response);
     }
 
@@ -44,7 +43,7 @@ public class BookingController {
     public ResponseEntity<BookingResponseDTO> getBooking(@PathVariable Long bookingId,
                                                  @RequestHeader("X-Sharer-User-Id") Long userId) {
         Booking booking = bookingService.getBooking(bookingId, userId);
-        BookingResponseDTO response = new BookingResponseDTO(booking);
+        BookingResponseDTO response = BookingResponseMapper.toBookingResponseDTO(booking);
         return ResponseEntity.ok(response);
     }
 
@@ -54,7 +53,7 @@ public class BookingController {
             @RequestParam(defaultValue = "ALL") BookingState state) {
         List<Booking> bookings = bookingService.getAllBookings(userId, state);
         List<BookingResponseDTO> bookingResponseDtos = bookings.stream()
-                .map(BookingResponseDTO::new)
+                .map(BookingResponseMapper::toBookingResponseDTO)
                 .toList();
         return ResponseEntity.ok(bookingResponseDtos);
     }
@@ -67,7 +66,7 @@ public class BookingController {
         List<Booking> bookings = bookingService.getBookingsForOwner(userId, state);
 
         List<BookingResponseDTO> bookingResponseDtos = bookings.stream()
-                .map(BookingResponseDTO::new)
+                .map(BookingResponseMapper::toBookingResponseDTO)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(bookingResponseDtos);
--- src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDTO.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDTO.java
@@ -1,8 +1,8 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.UserDto;
 
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BookingResponseDTO {
     private Long id;
     private LocalDateTime start;
@@ -17,26 +18,5 @@ public class BookingResponseDTO {
     private String status;
     private UserDto booker;
     private ItemDto item;
-
-    public BookingResponseDTO(Booking booking) {
-        this.id = booking.getId();
-        this.start = booking.getStart();
-        this.end = booking.getEnd();
-        this.status = booking.getStatus().name();
-
-        this.booker = new UserDto(
-                booking.getBooker().getId(),
-                booking.getBooker().getName(),
-                booking.getBooker().getEmail()
-        );
-
-        this.item = new ItemDto(
-                booking.getItem().getName(),
-                booking.getItem().getDescription(),
-                booking.getItem().isAvailable(),
-                booking.getItem().getRequestId(),
-                booking.getItem().getId()
-        );
-    }
 }
 
